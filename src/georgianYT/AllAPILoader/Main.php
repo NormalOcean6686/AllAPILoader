b1817e879a168f334e9ba1a35cfd316e17f6b56e
georgianYT <35131387+georgianYT@users.noreply.github.com>
2018-05-23 22:08:50 -0400

    Update Main.php

--- src/georgianYT/AllAPILoader/Main.php
+++ src/georgianYT/AllAPILoader/Main.php
@@ -10,15 +10,15 @@ use georgianYT\AllAPILoader\Loaders\AllPharPluginLoader;
 use georgianYT\AllAPILoader\Loaders\AllScriptPluginLoader;
 
 class Main extends PluginBase {
-
+  
     public function onEnable() {
         $this->getServer()->getPluginManager()->registerInterface(AllPharPluginLoader::class);
-
+      
         $this->getServer()->getPluginManager()->registerInterface(AllScriptPluginLoader::class);
-
+      
         if ($this->getServer()->getPluginManager()->getPlugin("DevTools") instanceof Plugin or $this->getServer()->getPluginManager()->getPlugin("FolderPluginLoader") instanceof Plugin)
             $this->getServer()->getPluginManager()->registerInterface(AllFolderPluginLoader::class);
-
+      
         $this->getServer()->getPluginManager()->loadPlugins($this->getServer()->getPluginPath(), [AllPharPluginLoader::class, AllScriptPluginLoader::class, AllFolderPluginLoader::class]);
         $this->getServer()->enablePlugins(PluginLoadOrder::STARTUP);
     }
